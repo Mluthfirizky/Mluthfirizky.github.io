20d0ce6990b0dc58f3ed943daba0842b06fdcff2
Mluthfirizky <luthfi.ginting@icloud.com>
2021-10-28 10:38:27 +0700

    Updates

---
+++ precache-manifest.5f0beffbd0c8844cfd40bf11ba02ed17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ee40bd2fb7cd8fd7351b27d1a2350c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87aef7a9e4804342962e",
+    "url": "/static/css/main.d0a0ea9f.chunk.css"
+  },
+  {
+    "revision": "c0ef98307eba545e5354",
+    "url": "/static/js/2.7c5ccc99.chunk.js"
+  },
+  {
+    "revision": "dc55280d2f5781f02381ff456c0225c7",
+    "url": "/static/js/2.7c5ccc99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87aef7a9e4804342962e",
+    "url": "/static/js/main.63165336.chunk.js"
+  },
+  {
+    "revision": "51b1263d09889fed3508",
+    "url": "/static/js/runtime-main.07ad49ca.js"
+  }
+]);
